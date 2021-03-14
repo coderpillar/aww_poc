ce64b255791fbf0ee941d175f5078c73f5939da4
Jacob G. Thibault <jacob.thibault@ampermusic.com>
2021-03-14 18:10:46 +0100

    made first ui changes to display random image of animal and random name with refresh button

--- flutter_ui/test/pet_finder_test.dart
+++ flutter_ui/test/pet_finder_test.dart
@@ -3,13 +3,13 @@ import 'package:flutter_ui/pet_finder.dart';
 
 void main() {
   group('PetFinder', () {
-    test('findName method should return non-zero lenght string', () {
+    test('findName method should return non-zero length string', () {
       final finder = PetFinder();
       var name = finder.findName();
       expect(name.length, isNot(equals(0)));
     });
 
-    test('findImage method should return non-zero lenght string', () {
+    test('findImageUrl method should return non-zero length string', () {
       final finder = PetFinder();
       var image = finder.findImageUrl();
       expect(image.length, isNot(equals(0)));
