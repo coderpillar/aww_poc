693d5b13b7b0d180b813a41d9b8e90cc06b68fc9
Jacob G. Thibault <jacob.thibault@ampermusic.com>
2021-03-14 23:03:14 +0100

    added a new screen, some navigation, buttons to home screen to pet the animal and adopt it, and a snackbar to show the pet's happiness when the user pets it

--- flutter_ui/lib/main.dart
+++ flutter_ui/lib/main.dart
@@ -15,21 +15,26 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: AdoptionPage(title: 'Pet Adoption Center'),
+      home: HomeScreen(title: 'Pet Adoption Center'),
+      initialRoute: '/home',
+      routes: {
+        '/home': (context) => HomeScreen(),
+        '/pets': (context) => PetsListScreen(),
+      },
     );
   }
 }
 
-class AdoptionPage extends StatefulWidget {
-  AdoptionPage({Key key, this.title}) : super(key: key);
+class HomeScreen extends StatefulWidget {
+  HomeScreen({Key key, this.title}) : super(key: key);
 
   final String title;
 
   @override
-  _AdoptionPageState createState() => _AdoptionPageState();
+  _HomeScreenState createState() => _HomeScreenState();
 }
 
-class _AdoptionPageState extends State<AdoptionPage> {
+class _HomeScreenState extends State<HomeScreen> {
   Pet _currentPet = Pet(PetFinder().findName(), PetFinder().findImageUrl());
 
   void _viewNewPet() {
@@ -37,6 +42,10 @@ class _AdoptionPageState extends State<AdoptionPage> {
       _currentPet.setName(PetFinder().findName());
       _currentPet.setImageUrl(PetFinder().findImageUrl());
     });
+  }
+
+  void _petCurrentPet() {
+    _currentPet.incrementTimesPet();
   }
 
   @override
@@ -58,18 +67,64 @@ class _AdoptionPageState extends State<AdoptionPage> {
               loadingBuilder: (context, child, progress) {
                 return progress == null ? child : LinearProgressIndicator();
               },
+              fit: BoxFit.contain,
             ),
             Text(
               _currentPet.name,
               style: Theme.of(context).textTheme.headline4,
             ),
+            FloatingActionButton(
+              onPressed: () {
+                _petCurrentPet();
+                final snackBar = SnackBar(
+                  content: Text(_currentPet.name + ' was happy to be pet :)'),
+                );
+                ScaffoldMessenger.of(context).showSnackBar(snackBar);
+              },
+              tooltip: 'Pet the Animal',
+              child: Icon(Icons.pets),
+            ),
+            ElevatedButton(
+                child: Text('Adopt This Pet'),
+                onPressed: () {
+                  Navigator.pushNamed(context, '/pets');
+                })
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _viewNewPet,
-        tooltip: 'Increment',
+        tooltip: 'Refresh',
         child: Icon(Icons.refresh),
+      ),
+    );
+  }
+}
+
+class PetsListScreen extends StatefulWidget {
+  PetsListScreen();
+
+  @override
+  _PetsListScreenState createState() => _PetsListScreenState();
+}
+
+class _PetsListScreenState extends State<PetsListScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My Pets'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Pets list goes here.',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
       ),
     );
   }
